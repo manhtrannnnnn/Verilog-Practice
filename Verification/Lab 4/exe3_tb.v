c1898fe1aab1a488aa4947a43903be90288b681f
manhtrannnnnn <manhdeptrainhat2003@gmail.com>
2024-12-22 16:04:07 +0700

    update 22/12

--- Verification/Lab 4/exe3_tb.v
+++ Verification/Lab 4/exe3_tb.v
@@ -33,25 +33,48 @@ module bidirectional_memory_tb();
         #10 wr_en = 1; addr = 4'h0; data_in = 16'hAAAA; #10 wr_en = 0;
         #10 wr_en = 1; addr = 4'h1; data_in = 16'h5555; #10 wr_en = 0;
         #10 wr_en = 1; addr = 4'h2; data_in = 16'h1234; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h3; data_in = 16'h2134; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h4; data_in = 16'h1022; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h5; data_in = 16'h3214; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h6; data_in = 16'h1325; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h7; data_in = 16'hAB12; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h8; data_in = 16'h0432; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'h9; data_in = 16'h1276; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'hA; data_in = 16'h4321; #10 wr_en = 0;
+        #10 wr_en = 1; addr = 4'hB; data_in = 16'h1135; #10 wr_en = 0;
+        
 
         // Read data from memory
         #10 rd_en = 1; addr = 4'h0; #10;
         #10 addr = 4'h1; #10;
-        #10 addr = 4'h2; #10 rd_en = 0;
-
-        // Check tri-state behavior
+        #10 addr = 4'h2; #10;
         #10 addr = 4'h3; #10;
+        #10 addr = 4'h4; #10;
+        #10 addr = 4'h5; #10;
+        #10 addr = 4'h6; #10;
+        #10 addr = 4'h7; #10;
+        #10 addr = 4'h8; #10;
+        #10 addr = 4'h9; #10;
+        #10 addr = 4'hA; #10; rd_en = 0;
+        
+        // Read nothing from memory
+        #10 rd_en = 1; addr = 4'hD; #10 
+        #10 addr = 4'hE; #10; 
+        #10 addr = 4'hB; #10; rd_en = 0;
+
+
       
-        //Display the result of module
-        if(failed == 0) begin
-          $display("============");
-          $display("TEST PASSED");
-          $display("============");
-        end
-        else begin
-          $display("===============");
-          $display("TEST FAILED | Case: %d", failed);
-          $display("===============");
+        // Display the result of the module
+        if (failed == 0) begin
+            $display("============");
+            $display("ALL TESTS PASSED");
+            $display("============");
+        end else begin
+            $display("===============");
+            $display("TEST FAILED");
+            $display("Total Passed: %d", passed);
+            $display("Total Failed: %d", failed);
+            $display("===============");
         end
 
         $finish;
@@ -66,20 +89,25 @@ module bidirectional_memory_tb();
     // Checker
     always @(posedge clk) begin
             if (wr_en) mem[addr] <= data_in;
-            if (rd_en) result(mem[addr]);
+      		if (rd_en) #1ns result(mem[addr]);
         end 
     
-  task result(input [15:0] expected_data_out);
-    	begin
-            if (expected_data_out == data_out) begin
+    task result(input [15:0] expected_data_out);
+        begin
+            if ((expected_data_out === 16'hXXXX) && (data_out === 16'hXXXX)) begin
                 passed = passed + 1;
-            end
-            else begin
-                $display("Test Failed || Read addr: %b from memory || Expected data out: %b || Data out: %b", addr, expected_data_out, data_out);
+                $display("[Time : %0t] Test Passed || Addr: %d || Both expected and data_out are undefined (XXXX)",
+                         $time, addr);
+            end else if (expected_data_out === data_out) begin
+                passed = passed + 1;
+            end else begin
+                $display("[Time : %0t] Test Failed || Addr: %d || Expected: %h || Got: %h", 
+                         $time, addr, expected_data_out, data_out);
                 failed = failed + 1;
             end
         end
     endtask
+
     
     // Monitor 
     initial begin
